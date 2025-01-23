4436df4464f8af486874463dd1ee913e8b380265
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-01-23 02:40:24 +0000

    lab.c outlined

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,35 @@
-/**Update this file with the starter code**/
+/**
+ * File: lab.c
+ * Author: MTumir
+ * Created: 01.22.2025
+ * Latest: 01.22.2025
+ * 
+ * Description: Implements functions defined in lab.h.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#include "lab.h"
+
+list_t *list_init(void (*destroy_data)(void *), int (*compare_to)(const void *, const void *)) {
+
+}
+
+void list_destroy(list_t **list) {
+
+}
+
+list_t *list_add(list_t *list, void *data) {
+
+}
+
+void *list_remove_index(list_t *list, size_t index) {
+
+}
+
+int list_indexof(list_t *list, void *data) {
+
+}
